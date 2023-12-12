0590fff42d69c12961d7d2be3da33ad6a9d1c7e8
nekiwanuka <nekiwanuka@gmail.com>
2023-12-12 18:31:58 +0300

    Add Superuser

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,7 @@ from .managers import UserManager
 from rest_framework_simplejwt.tokens import RefreshToken
 
 
-AUTH_PROVIDERS={'email':'email', 'google':'google', 'facebook':'facebook'}
+AUTH_PROVIDERS = {"email": "email", "google": "google", "facebook": "facebook"}
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -21,7 +21,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(auto_now=True)
     last_login = models.DateTimeField(auto_now=True)
-    auth_provider=models.CharField(max_length=50, default=AUTH_PROVIDERS.get('email'))
+    auth_provider = models.CharField(max_length=50, default=AUTH_PROVIDERS.get("email"))
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["first_name", "last_name"]
 
@@ -43,5 +43,6 @@ class OneTimePassword(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     code = models.CharField(max_length=6, unique=True)
 
+
     def __str__(self):
-        return f"Passcode for {self.user.get_full_name()}"
+        return f"Passcode for {self.user.get_full_name}"
