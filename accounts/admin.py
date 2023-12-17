b32d31237be57fddff2cad21a397434f738d88e0
nekiwanuka <nekiwanuka@gmail.com>
2023-12-17 13:46:18 +0300

    Serve static

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,14 +1,50 @@
 # accounts/admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User
+from .models import User, Admin, Contributor, EmailVerification
+
 
 class UserAdmin(BaseUserAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_contributor', 'last_login')
-    list_filter = ('is_staff', 'is_contributor', 'is_active')
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Personal Info', {'fields': ('first_name', 'last_name')}),
-        ('Permissions', {'fields': ('is_staff', 'is_contributor', 'is_active')}),
-   
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_contributor",
+        "last_login",
     )
+    list_filter = ("is_staff", "is_contributor", "is_active")
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal Info", {"fields": ("first_name", "last_name")}),
+        ("Permissions", {"fields": ("is_staff", "is_contributor", "is_active")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_contributor",
+                    "is_active",
+                ),
+            },
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
+
+
+# Register the User model with the custom UserAdmin
+admin.site.register(User, UserAdmin)
+
+# Register other models
+admin.site.register(Admin)
+admin.site.register(Contributor)
+admin.site.register(EmailVerification)
